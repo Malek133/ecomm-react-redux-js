50125e877732f70cff1341791421c25f6b2e3358
unknown <amazighmalek.com>
2024-07-10 11:13:09 +0200

    codes

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,19 +3,14 @@ import ProductCard from '../components/ProductCard'
 import axios from 'axios';
 import {useQuery} from  '@tanstack/react-query'
 import ProductSkelaton from '../components/ProductSkelaton'
-// import { useNavigate } from 'react-router-dom';
-// import {useColorMode} from '@chakra-ui/color-mode';
 import { useEffect } from "react";
 
 const ProductPage = () => {
 
-    // const {id} = useParams();
-    // const navigate = useNavigate()
-    // const { colorMode } = useColorMode();
-
     const getProductList = async () =>{
         const {data} = await 
         axios.get(`${import.meta.env.VITE_SERVER_URL}/api/products?populate=categories,thumbnail`);
+        
         return data;
       };
 
@@ -29,6 +24,8 @@ const ProductPage = () => {
       useEffect(() => {
         document.title=`Product ${data?.data?.attributes?.title} Page`;
                }, [data])
+
+    
       
       if(isLoading) return <Grid margin={10}  
       templateColumns='repeat(auto-fill,minmax(300px,1fr) )' gap={6}>{
@@ -36,14 +33,13 @@ const ProductPage = () => {
            <ProductSkelaton key={i} />)
       )}</Grid>;
 
-   
-
+  
    
   return (
     <Grid margin={10} 
     templateColumns='repeat(auto-fill,minmax(340px,1fr) )' gap={6}>
         {
-            data.data.map(product => (
+           data.data && data.data.map(product => (
              <ProductCard key={product.id}   {...product} />
             ))
         }
@@ -56,3 +52,5 @@ const ProductPage = () => {
 }
 
 export default ProductPage
+
+
